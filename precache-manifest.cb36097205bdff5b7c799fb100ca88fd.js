e70e9641c0df2954d9487d29473e7150a52b4b2f
NourSofanati <ultradraftns@gmail.com>
2019-06-11 20:33:45 +0300

    files

---
+++ precache-manifest.cb36097205bdff5b7c799fb100ca88fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b150538da7a71398d44fd39c440f9889",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ec134ccd3075492e8c6",
+    "url": "/static/css/main.41c69348.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "2ec134ccd3075492e8c6",
+    "url": "/static/js/main.14f000e0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "717808fab08cc8976427740ce7ee077b",
+    "url": "/static/media/bg.717808fa.jpg"
+  }
+]);
